3ab1c3b8f817ad2fe4266b5298dba47dd4b95df6
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-10-17 17:48:13 +0200

    guide lines Java

--- src/App.java
+++ src/App.java
@@ -1,8 +1,17 @@
 public class App {
+    // all'interno dell' codice ci deve essere una funzione main che e' il punto d'entrata da cui il codice inizia
     public static void main(String[] args) throws Exception {
         int x = 4;
+        // Java e' un linguaggio che richiede la tipizazzione delle variabili
+        // inizializzione e assegnazione variabile uguale a 4 di tipo intero
         int y = 5;
         int z = x + y;
+
+        
+        // if else sono a livello di sinstassi e' simile a JS\
+        // per quanto riguarda la visualizzazione del codice in console
+        // si usa System.out.println 'println' dice al codice di andare a capo
+        // se fosse solo System.out.print il codice in console non va a capo
 
         if(z % 2 == 0){
             System.out.println("the number is Even");
@@ -10,5 +19,9 @@ public class App {
         else{
             System.out.println("the number is Odd");
         }
+
+        // TIPI DI DATI 
+
+        
     }
 }
