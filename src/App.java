f5b18f5c9b782111a1da8ae03eb37d9c39f1504b
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-10-17 16:10:17 +0200

    initial commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x = 4;
+        int y = 5;
+        int z = x + y;
+
+        if(z % 2 == 0){
+            System.out.println("the number is Even");
+        }
+        else{
+            System.out.println("the number is Odd");
+        }
+    }
+}
