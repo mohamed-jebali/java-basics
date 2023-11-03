af9360a69fc83cd673644af66ea037ff87b20fdd
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-11-03 17:19:16 +0100

    exercise 10 complete

--- src/Exercise10.java
+++ src/Exercise10.java
@@ -9,13 +9,35 @@ public class Exercise10 {
         int InputNumber = input.nextInt();
         
 
-        for (int i = 1; i <= InputNumber; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(j);
-                System.out.println();
+        for (int i = 1; i <= InputNumber; i++) { // primo for incrementa di 1 l'input dell'utente fino al numero inserito incluso
+            for (int k = i; k > 1; k--) { // for innestato decrementa di 1 i numeri prodotti dall utente fino a 1 lo zero e' eslcuso
+                System.out.print(k);
             }
+             for (int t = 1; t <= i; t++) { // secondo for innestato incrementa di 1 l'input la variabile i prodotta dall'input dell'utente
+                                            // si dichiara una variabile t uguale a 1 che e' minore o uguale ad i variabile prodotto dall'utente
+                System.out.print(t);
+            }
+
+            System.out.println(""); // per ogni iterazione la console va a capo
         }
-    
+        ///
+
+        for (int i = InputNumber - 1; i > 0; i--) { // primo for incrementa di 1 l'input dell'utente fino al numero inserito incluso
+            for (int k = i; k > 1; k--) { // for innestato decrementa di 1 i numeri prodotti dall utente fino a 1 lo zero e' eslcuso
+                System.out.print(k);
+            }
+             for (int t = 1; t <= i; t++) { // secondo for innestato incrementa di 1 l'input la variabile i prodotta dall'input dell'utente
+                                            // si dichiara una variabile t uguale a 1 che e' minore o uguale ad i variabile prodotto dall'utente
+                System.out.print(t);
+            }
+
+            System.out.println(""); // per ogni iterazione la console va a capo
+        }
+        ///
+
+
+
+
     
     
         input.close();
