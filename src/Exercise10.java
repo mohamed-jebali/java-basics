e66c18bd2cbacd28848c4f2cbfc614a40c16b86d
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-11-03 15:33:12 +0100

    exercise 10 complete

---
+++ src/Exercise10.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercise10 {
+    public static void main (String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Inserisci il numero di righe");
+
+        int InputNumber = input.nextInt();
+        
+
+        for (int i = 1; i <= InputNumber; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+                System.out.println();
+            }
+        }
+    
+    
+    
+        input.close();
+
+    }
+}
